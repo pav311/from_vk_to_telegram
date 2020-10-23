1513846ce299b6269fa8bae507e124553c215fe6
Paul <49080975+pav311@users.noreply.github.com>
2020-10-24 03:16:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import time
 import telebot
 import io
 
-##############################################
-bot_token=''###str### Token of vk page.
-vk_community_id=-1###int### ID of community
-vk_token=''###str### Token of vk page
-###############################################
+
+bot_token=''
+vk_community_id=-1
+vk_token=''
+telegram_id_channel=''
 
 
 vk_session = vk_api.VkApi(token=vk_token)
@@ -19,7 +19,7 @@ post_count_before = vk.wall.get(owner_id=vk_community_id).get("count")
 
 def send_message_to_channel(post_text):
     url = "https://api.telegram.org/bot{}/sendMessage".format(bot_token)
-    data = {'chat_id' : "@sdp_dl", "text" : post_text}
+    data = {'chat_id' : telegram_id_channel, "text" : post_text}
     r= requests.post(url,data=data)
     return r
 
@@ -29,7 +29,7 @@ def send_photo_to_channel(img_url,post_text=""):
     photo = io.BytesIO(remote_image.content)
     photo.name = 'img.png'
     files = {'photo': photo}
-    data = {'chat_id' : "@sdp_dl", "caption" : post_text}
+    data = {'chat_id' : telegram_id_channel, "caption" : post_text}
     r= requests.post(url, files=files, data=data)
     return r
 
@@ -44,9 +44,11 @@ while True:
     if post_count_before < post_count:
         if vk.wall.get(owner_id=vk_community_id).get("items")[1].get("attachments")!=None:
             last_post = vk.wall.get(owner_id=vk_community_id).get("items")[0].get("is_pinned")
+            
             if last_post == 1:
                 img_url=(vk.wall.get(owner_id=vk_community_id).get("items")[1].get("attachments")[0].get("photo").get("sizes")[-1].get("url"))
                 post_text=(vk.wall.get(owner_id=vk_community_id).get("items")[1].get("text"))
+                
             elif last_post==None:
                 img_url=(vk.wall.get(owner_id=vk_community_id).get("items")[0].get("attachments")[0].get("photo").get("sizes")[-1].get("url"))
                 post_text=(vk.wall.get(owner_id=vk_community_id).get("items")[0].get("text"))
@@ -55,8 +57,10 @@ while True:
 
         elif vk.wall.get(owner_id=vk_community_id).get("items")[1].get("attachments")==None:
             last_post = vk.wall.get(owner_id=vk_community_id).get("items")[0].get("is_pinned")
+            
             if last_post == 1:
                 post_text=(vk.wall.get(owner_id=vk_community_id).get("items")[1].get("text"))
+                
             elif last_post==None:
                 post_text=(vk.wall.get(owner_id=vk_community_id).get("items")[0].get("text"))
             send_message_to_channel(post_text=post_text)
